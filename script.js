f28b17919383edb443c4577f4587bfc49bc9684e
fjalafoo <fjalafoo@gmail.com>
2022-10-03 14:33:52 +0300

    Clickable circles

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
+let gameboard = document.querySelector('.circles')
 
 
+gameboard.addEventListener('click', handleClick)
 
+function handleClick(event){
+   console.log(event.target.id)
+ 
+}
 
 
 
